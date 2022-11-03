118824e3eb0bca34c4c1a7cf1d3a2e3a229e9b88
shajin <shajinvijay92@gmail.com>
2022-11-03 16:57:42 +0530

    first cmd

---
+++ hello.py
@@ -0,0 +1,21 @@
+class Hello_Name():
+    
+    def __init__(self,name):
+        self.name = name
+        
+        
+    def run_hello(self):
+        print("my name is", self.name)
+        
+obj =  Hello_Name("shajin")
+
+obj.run_hello()      
+            
+        
+        
+        
+    
+    
+        
+        
+        
