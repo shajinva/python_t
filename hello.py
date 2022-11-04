8dafa4c0a0b988e45b5b4d04b136ac979a0209ae
shajin <shajinvijay92@gmail.com>
2022-11-04 13:34:18 +0530

    first cmlast sdsd sadas

--- hello.py
+++ hello.py
@@ -1,18 +1,19 @@
 class Hello_Name():
     
-    def __init__(self,name):
+    def __init__(self,name,age):
         self.name = name
-        
+        self.age = age        
         
     def run_hello(self):
         print("my name is", self.name)
         
-    def run_hello(self):
-        print(self.name)
+    def run_age(self):
+       print("my age is :" , self.age)
         
 obj =  Hello_Name("shajin",45)
 
-obj.run_hello()      
+obj.run_hello()  
+obj.run_age()     
             
         
         
